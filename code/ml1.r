f9b9c2440800b45cb75a8f61b917c6b77e824ef1
Jared Lander <info@jaredlander.com>
2022-06-24 17:20:35 +0000

    Started recipes

--- code/ml1.r
+++ code/ml1.r
@@ -26,3 +26,39 @@ test <- testing(credit_split)
 train
 
 # EDA ####
+
+library(ggplot2)
+ggplot(train, aes(x=Status)) + geom_bar()
+
+# need to balance good/bad
+
+ggplot(train, aes(x=Amount, y=Status, color=Job)) + geom_point()
+
+ggplot(train, aes(x=Amount, y=Age, color=Status)) + geom_point()
+
+# Choose Our First Model ####
+
+# penalized regression
+# boost tree
+
+# Preprocessing ####
+
+library(recipes)
+
+# outcome: response, label, target, dependent variable (crappy term), y
+# input: predictor, covariate, feature, x
+
+train |> count(Home)
+
+recipe(Status ~ ., data=train) |>
+    step_factor2string(Home, Job) |>
+    step_mutate(Home=if_else(is.na(Home), 'Missing', Home)) |>
+    step_mutate(Job=if_else(is.na(Job), 'Missing', Job)) |>
+    step_string2factor(Home, Job) |>
+    # step_other(Home, other='misc') |>
+    step_other(all_nominal_predictors(), other='misc') |>
+    # step_dummy(Home, keep_original_cols=FALSE, one_hot=TRUE) |>
+    # step_dummy(Job, one_hot=TRUE) |>
+    step_dummy(all_nominal_predictors(), one_hot=TRUE) |>
+    prep() |>
+    bake(new_data=NULL)
